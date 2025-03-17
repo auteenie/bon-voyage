10db1af71909ce3207b013b794b6075e68beb888
Athena Chang <athenachang17@gmail.com>
2025-03-16 23:37:59 -0400

    rRevised HTML elements and added className in CountriesPage. Started adding a filter option.

--- app/src/pages/CountriesPage.jsx
+++ app/src/pages/CountriesPage.jsx
@@ -1,30 +1,41 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import FlagsList from "../components/FlagsList";
+import NavBar from "../components/NavBar";
 
 const CountriesPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <div>
-        <Button
-          name="View Account"
-          onClick={() => navigate("/pages/PassportPage")}
-        />
+    <main className="view-countries">
+      <NavBar className="countries-nav">
+        <li>
+          <Button
+            name="View Account"
+            onClick={() => navigate("/pages/PassportPage")}
+          />
+        </li>
 
         <h1>List of Countries</h1>
+      </NavBar>
+
+      <div className="search-filter">
         <input
           type="text"
           name="search"
+          className="search-bar"
           placeholder="Search for a Country..."
         />
 
-        <Button name="Log Out" />
+        <select>
+          <option></option>
+        </select>
       </div>
 
-      <FlagsList />
-    </div>
+      <section className="flags-list">
+        <FlagsList />
+      </section>
+    </main>
   );
 };
 
