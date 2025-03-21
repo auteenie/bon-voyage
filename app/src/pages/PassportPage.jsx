5c28f547f6a3001f63568d56066f0f6c7379cc32
Athena Chang <athenachang17@gmail.com>
2025-03-21 01:57:50 -0400

    minor revision on PassportPage

--- app/src/pages/PassportPage.jsx
+++ app/src/pages/PassportPage.jsx
@@ -7,20 +7,21 @@ import { Earth, ArrowLeft, ArrowRight } from "lucide-react";
 import stamps from "../stamps.js";
 
 const PassportPage = () => {
-  const navigate = useNavigate();
-  const { userID } = useParams();
-  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
   const [isOpen, setIsOpen] = useState(false);
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedCountry, setSelectedCountry] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const navigate = useNavigate();
+  const { userID } = useParams();
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
   const stampsArray = Object.entries(stamps);
   const stampsPerPage = 6;
   const totalPages = Math.ceil(stampsArray.length / (stampsPerPage * 2));
 
   useEffect(() => {
     if (!currentUser) {
-      navigate('/');
+      navigate("/");
       return;
     }
 
@@ -28,7 +29,7 @@ const PassportPage = () => {
     console.log("Does this match current user?", currentUser.id === userID);
 
     if (currentUser.id !== userID) {
-      navigate('/');
+      navigate("/");
     }
   }, [currentUser, userID, navigate]);
 
@@ -46,7 +47,7 @@ const PassportPage = () => {
 
     return (
       <div className="passport-page">
-        {pageStamps.map(([country, stampSrc], idx) => (
+        {pageStamps.map(([country, stampSrc]) => (
           <div
             key={country}
             className="stamp-container"
@@ -56,7 +57,6 @@ const PassportPage = () => {
             }}
           >
             <img className="stamp" src={stampSrc} alt={`${country} stamp`} />
-            {/* <p>{country}</p> */}
           </div>
         ))}
       </div>
